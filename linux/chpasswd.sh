6879aeb3286a86d77d245227e55050dd147ef14c
Landon Beach <LandonBeach@users.noreply.github.com>
2017-03-20 15:14:54 -0600

    fixed problem with 'passwd -a' option.

--- linux/chpasswd.sh
+++ linux/chpasswd.sh
@@ -8,9 +8,18 @@
 
 echo -e "\n\t To skip a user set the password to 0."
 
+# Get a list of user accounts WITH a password set (users_wp)
+# and a list of users WITHOUT a password set (users_np). 
+# NOTE: Does not include system accounts.
+users_wp=()
+users_np=()
+for user in $(cut -d ":" -f1 /etc/passwd); do
+   users_wp+=($(sudo passwd -S $user | awk '$2 == "PS" || $2 == "P" {print $1}'))
+   users_np+=($(sudo passwd -S $user | awk '$2 == "NP" {print $1}'))
+done
+
 # Prompt to change the password for all user accounts, except root.
-users=$(passwd -aS | awk '$2 == "P" || $2 == “NP” { print $1 }')
-for user in $users; do
+for user in ${users_wp[*]} ${users_np[*]}; do
    if [ $user != "root" ]; then
        read -p "Type new password for $user: " pass
        if [ "$pass" == "0" ]; then
@@ -21,6 +30,7 @@ for user in $users; do
            read -p "Lock account [y/n]? " lock
            if [ "$lock" == "y" ]; then
                passwd -l $user
+               # chage -E 0 $user
                echo "$user has been locked!"
            fi
        fi
@@ -28,13 +38,13 @@ for user in $users; do
 done
 
 # Automatically lock user accounts without a password.
-users=$(passwd -aS | awk '$2 == "NP" { print $1 } ')
-for user in $users; do
+for user in ${users_np[*]}; do
     if [ $user != "root" ]; then
         echo "$user doesn't have a password!"
         echo -n "Locking account... "
         passwd -l $user
+        #chage -E 0 $user
         echo "done!"
     fi
 done
-unset user users lock pass
+unset user users_wp users_np lock pass
