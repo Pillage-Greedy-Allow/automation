55f8fa1b1cb43895d5a7e97efcae438d40916aed
ZachChristensen28 <zachchristensen28@gmail.com>
2018-03-23 10:36:01 -0600

    Added iptable logging and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# automation
-Scripts to automate all the things. Useful scripts for competitons.
+# Automation
+
+Scripts to automate all the things. Useful scripts for competitions.
+
+# Iptables
+
+`iptables.sh` must be configured before running automate.sh. Basic logging has been added to a few iptable rules. Monitor /var/log/kern.log (Debian) to find potential threats or to figure out how the scoring engine works.
+
+**Useful command** `sudo tailf /var/log/kern.log | grep '*'`
+> If you have your grep color set to auto then this will do some really nice highlighting for the iptables flags.
+
+## Logging
+
+Mastering the art of log monitoring will greatly increase your skills at competition. Creating custom iptable logs will help you out with figuring out the scoring engine and potential threats.
+
+To log in iptables use the `-j LOG` flag. To add helpful flags use `-j LOG --log-prefix '** FLAG HERE ** '`.
+
+i.e.
+> `iptables -INPUT -s 127.0.0.1 -j LOG --log-prefix '** LOCALHOST ** '` adds a flag for local host traffic.
--- linux/iptables.sh
+++ linux/iptables.sh
@@ -25,11 +25,13 @@ iptables -A INPUT -m recent --name BLACKLIST --rsource --remove
 
 # Drop invalid packets (protects against fragmented packets and MOST scans).
 # Accept connections already related and established.
+iptables -A INPUT -m conntrack --ctstate INVALID -j LOG --log-prefix '** POTENTIAL SCAN ** '
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # Send new TCP traffic starting with a SYN to the TCP chain, else drop.
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
+iptables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j LOG --log-prefix '** POTENTIAL TCP SCAN ** '
 iptables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
 
 # Send new UDP traffic to the UDP chain.
@@ -39,25 +41,32 @@ iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p icmp -j ICMP
 
 # TCP rules. Protects from SYN scans and floods.
-##### THIS is where you can add additional TCP rules for specific ips/ports needed (at top). ###### 
+##### THIS is where you can add additional TCP rules for specific ips/ports needed (at top). ######
+iptables -A TCP -p tcp --dport 22 -j LOG --log-prefix '** SSH ** '
 iptables -A TCP -p tcp --dport 22 -j ACCEPT
 iptables -A TCP -p tcp -m multiport --dports 23,79 --tcp-flags ALL SYN -j BLOCK
 iptables -A TCP -m limit --limit 2/s --limit-burst 6 -j RETURN
+iptables -A TCP -j LOG --log-prefix '** TCP BLOCKED ** '
 iptables -A TCP -j BLOCK
 
 # UDP rules. Protects from packets with no content and floods.
-##### THIS is where you can add additional UDP rules for specific ips/ports needed (at top). ###### 
+##### THIS is where you can add additional UDP rules for specific ips/ports needed (at top). ######
+iptables -A UDP -p udp -m length --length 1:28 -j LOG --log-prefix '** POTENTIAL UDP FLOOD ** '
 iptables -A UDP -p udp -m length --length 1:28 -j DROP
 iptables -A UDP -m limit --limit 10/s --limit-burst 20 -j RETURN
+iptables -A UDP -j LOG --log-prefix '** UDP BLOCKED ** '
 iptables -A UDP -j BLOCK
 
 # ICMP rules. Accepts echo requests and protects from floods.
+iptables -A ICMP -p icmp -j LOG --log-prefix '** PING ** ' # can be helpful to find scoring engine
 iptables -A ICMP -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A ICMP -m limit --limit 4/s --limit-burst 8 -j RETURN
+iptables -A ICMP -j LOG --log-prefix '** PING BLOCKED ** '
 iptables -A ICMP -j BLOCK
 
 # Update the timestamp on the offending packet, if offender already exists in the blacklist.
 # Add them to the blacklist if they're not already in the blacklist.
+iptables -A BLOCK -m recent --name BLACKLIST --rsource -j LOG --log-prefix '** BLACKLIST ** ' # helpful for debugging and finding attackers
 iptables -A BLOCK -m recent --update --seconds 180 --name BLACKLIST --rsource -j DROP
 iptables -A BLOCK -m recent --set --name BLACKLIST --rsource -j DROP
 
